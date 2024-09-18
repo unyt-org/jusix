3e153577cbccbc6ae43cd5269d8ee7940922ea45
Benedikt Strehle <benedikt.strehle@uni-ulm.de>
2024-09-19 00:05:24 +0200

    update uix-tests

--- uix-tests/common/Component1.tsx
+++ uix-tests/common/Component1.tsx
@@ -1,13 +1,29 @@
+import { PointerWithPrimitive, Ref } from "datex-core-legacy/runtime/pointers.ts";
+import { inferType } from "datex-core-legacy/types/struct.ts";
 import { Component } from 'uix/components/Component.ts';
 
-@template(({value, data}) => {
-	console.log("value", value.val)
+export const Car = struct({
+	brand: string,
+	model: string,
+	year: number
+});
+type Car = inferType<typeof Car>;
+
+@template(({number1, number2, boolean, data, data2, car}) => {
+	console.log("value", number1.val, car, data.a, data2?.$.a, car?.$.brand);
 	return <>
-		<div>Current value: <b>{value}</b></div>
-		<div>Current value * 10: <b>{value * 10}</b></div>
+		<div>Current value: <b>{number1}</b></div>
+		<div>Current value * 10: <b>{number1 * 10}</b></div>
 		<div>A: {data.a}</div>
 	</>
 })
-export default class Component1 extends Component<{ value: number, data: {a: number} }> {
+export default class Component1 extends Component<{ 
+	number1: Ref<number>,
+	number2?: number,
+	boolean?: Ref<boolean>,
+	data: {a: number}, 
+	data2?: Ref<{a: number}>,
+	car?: Car 
+}> {
 
 }
--- uix-tests/common/tests.tsx
+++ uix-tests/common/tests.tsx
@@ -1,4 +1,5 @@
-import Component1 from "common/Component1.tsx";
+import Component1, { Car } from "common/Component1.tsx";
+import { ObjectRef, Ref } from "datex-core-legacy/runtime/pointers.ts";
 
 export default {
 
@@ -16,18 +17,52 @@ export default {
 			data2.a = value;
 		});
 
-		let x = 1;
+		let x = $$(1);
 
-		return <div>
-			<input type="number" value={value}/>
-			<Component1 value={value} data={data}/>
+		const classes = $$(["a", "b"]);
+		setInterval(() => {
+			classes.push("c_" + Math.round(Math.random()*100));
+		}, 1000);
+
+		const classesObj = $$({a: true, b: false, c:4});
+		setInterval(() => {
+			classesObj.a = !classesObj.a;
+			classesObj.b = !classesObj.b;
+		}, 1000);
+
+		return <div id="xy" class={$$("xyz")}>
+			<input
+				class={classes}
+				disabled={$$(false)}
+				required={true}
+				type={$$("text" as const)} 
+				value={value}
+				onclick={(e) => {
+					console.log("click", e);
+				}}
+				onclick:frontend={(e) => {
+					console.log("frontend click", e);
+				}}
+			/>
+			
+			<input class={['3',5,true]} type="number" value={'xy'}/>
+			<input type={"button"} value={$("fe")} style={$$({color: 'red', x: [3]})}/>
+
+			<x-custom-element x="234" y={5}>x</x-custom-element>
+			<Component1 number1={value} number2={value} data={data}/>
 			<hr/>
-			<Component1 value={value} data={data2}/>
+			<Component1 number1={value} number2={5} data={data2}/>
 			<hr/>
-			<Component1 value={x} data={data2}/>
+			<Component1 number1={x} data={data2} car={Car({
+				brand: "Toyota",
+				model: "Corolla",
+				year: 2012
+			})}/>
 		</div>
 
 
-	}
+	},
+
+	'/test3': () => <input type="number" value={Promise.resolve('xy')}/>
 
 }
